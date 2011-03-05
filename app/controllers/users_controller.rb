a98dc7ec5c612a0b78f5868597c30f8cd63f09f2
Vishal singh <vishal.singh030@gmail.com>
2011-03-05 15:53:59 +0530

    few changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def show
+     @user = current_user
+  end
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
